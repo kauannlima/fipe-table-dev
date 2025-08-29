9456e45f7f9bec3d81274ef96ea2408e5ec02cc3
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-08-29 15:10:10 -0300

    Corrigindo Hooks não utilizados

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const AboutPage = () => (
   <div className="max-w-2xl mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
     <h1 className="text-2xl font-bold mb-4">Sobre o Projeto</h1>
